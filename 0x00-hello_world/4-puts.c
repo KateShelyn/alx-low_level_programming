1ea62eb48167a5230a019b4c6d7138328f0251aa
KateShelyn <kateshelyn@gmail.com>
2022-09-08 12:16:14 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
-*main - print script puts function
-*Description - using the main function
-*C program that prints exactly Programming is like building a multilingual
-*return: 0
+* main - print script puts function
+*
+* Description - using the main function
+* C program that prints exactly Programming is like building a multilingual
+* Return: 0
 */
 int main(void)
 {
