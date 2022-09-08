209864ead0e022bf76f7124be508b6c8ab4e9ce4
KateShelyn <kateshelyn@gmail.com>
2022-09-08 14:55:57 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
-* main - print the string sizes
+* main- print the string int the printf function
 *
-* Description- using the main function 
+* Description- using the main function
+*
+* this program prints the various sizes of types
 * Return: 0
 */
 int main(void)
@@ -13,7 +15,6 @@ int main(void)
 	long li;
 	long long lli;
 	float f;
-
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
