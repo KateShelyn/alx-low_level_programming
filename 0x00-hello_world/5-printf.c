32d4fdad8af862da9f3ee40ce4a5636e6d1751cb
KateShelyn <kateshelyn@gmail.com>
2022-09-08 12:26:47 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print script printf function
+*
+* Description - using the main function
+* C program that prints exactly with proper grammar, but
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0)
+}
