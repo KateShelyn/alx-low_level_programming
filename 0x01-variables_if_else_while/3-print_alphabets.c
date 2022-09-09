f6e63557b8275e758153050b091677b67a836a8c
KateShelyn <kateshelyn@gmail.com>
2022-09-09 15:43:30 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <studio.h>
+/**
+ * main - print if number
+ *
+ * Description - using main function
+ * this program prints
+ * Reaturn: 0
+ */
+int main (void)
+{
+	char c;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
