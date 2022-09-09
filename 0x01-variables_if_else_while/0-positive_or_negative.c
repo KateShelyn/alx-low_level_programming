7ba75eea5d5d3cca35ead1443ed1e9f196d47f28
KateShelyn <kateshelyn@gmail.com>
2022-09-09 07:59:45 -0700

    task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* main -print script printf function*/
+
+/* Decription - using the main function*/
+
+/* this program will assign a random number to the variable  n*/
+int main(void)
+{
+	int array_var[n]
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* initialize all elements of my array*/
+	for (n = 0; n < 0; n++)
+	{
+	printf("%d", \n);
+	}
+	return (0);
