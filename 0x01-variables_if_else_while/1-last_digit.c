79865835bc611a7e3368f4d7d915a76858c9f845
KateShelyn <kateshelyn@gmail.com>
2022-09-09 16:39:03 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,7 +27,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d id %d and is less than6 and not 0\n", n, l);
+		printf("Last digit of %d id %d and is less than6 and not 0\n",n)
 	}
 	return (0)
 }
