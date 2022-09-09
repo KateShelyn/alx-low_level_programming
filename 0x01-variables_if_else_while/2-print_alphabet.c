ce6a578d443dc978f64d55f88fc9e0a781009efc
KateShelyn <kateshelyn@gmail.com>
2022-09-09 16:51:11 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main-print function
+*
+* description: using main function
+*this is  program
+* Return:0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
