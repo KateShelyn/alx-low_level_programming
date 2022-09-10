1b5d62f451ce06deb7530325569672948e609889
KateShelyn <kateshelyn@gmail.com>
2022-09-09 17:39:32 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 /**
- * main - print if the number is positive
- *
- * Description - using main function
- * this program prints
- * Return: 0
- */
+* main - print function
+*
+* description - uding the main function
+* this program prints
+* Return: 0
+*/
 int main(void)
 {
-	char c;
-	for (c = '0'; c <= '9'; c++)
-	{
-		putchar(c);
-	}
-	for (c = 'a'; c <= 'f'; c++)
-	{
-		putchar(c);
-	}
-	putchr('\n');
-	return (0);
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+	putchar(c);
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
 }
