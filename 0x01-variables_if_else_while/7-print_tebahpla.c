2168de71ae95ee35e078a63945bb49da83ce4a72
KateShelyn <kateshelyn@gmail.com>
2022-09-09 17:29:26 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - print function
+*
+* description - using main function
+* this program prints
+* Return: 0
+*/
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
