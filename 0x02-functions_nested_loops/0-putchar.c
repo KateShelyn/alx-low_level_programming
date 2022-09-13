2e4d1275325e6e40feba6ac2a5ff8b53981c46b7
KateShelyn <kateshelyn@gmail.com>
2022-09-13 07:30:25 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ * main - print function
+ * Description - using main function
+ * this program prints
+ * Return: 0
+ */
+int main(void)
+{
+	int;
+
+	putchar('\n');
+	return (0);
+}	
