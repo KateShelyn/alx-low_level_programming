ba313bc0f6bce0f364216a9b65017e0dc7119020
KateShelyn <kateshelyn@gmail.com>
2022-09-14 15:56:19 -0700

    updates

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,19 +10,20 @@ void jack_bauer(void)
 {
 	int h = 0;
 	int m = 0;
-	int h % = "h % 10";
-	int m % = "m % 10";
+	int hours_remainder = hours % 10;
+	int min_remainder = mins % 10;
 
 	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
 		{
 			_putchar((h / 10) + "0");
-			_putchar((h % ) + "0");
+			_putchar(hours_remainder + "0");
 			_putchar(":");
 			_putchar((m / 10) + "0");
-			_putchar((m % ) + "0");
+			_putchar(min_remainder + "0");
 			_putchar('\n');
+			minutes = 0;
 		}
 	}
 }
