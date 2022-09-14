00965ebaa2c2c8712d6030b2ed73ff4e125eba09
KateShelyn <kateshelyn@gmail.com>
2022-09-14 13:10:45 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,13 @@
 /**
  * _islower - function checks
  * Description - using _islower function
+ * @c: input  letter
  * this program prints
  * Return: 0
  */
 int _islower(int c)
 {
-	if (c >= 'a' && c<= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
