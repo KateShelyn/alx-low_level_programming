8dc396a7e89aa4558d78ac43299af98be2159946
KateShelyn <kateshelyn@gmail.com>
2022-09-14 11:33:50 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print the alphabet
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	_putchar('\n');
+}
