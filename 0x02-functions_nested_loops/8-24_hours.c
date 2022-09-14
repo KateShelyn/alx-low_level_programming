df9e7cc2c39831599445125123a85a0a51c9b1a2
KateShelyn <kateshelyn@gmail.com>
2022-09-14 16:03:09 -0700

    updates

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,28 +2,27 @@
 /**
  * jack_bauer - function
  * Description - using jack_bauer function
- * @h - hour
- * @m - minutes
+ * @hours - hours
+ * @min - minutes
  * Return: 0
  */
 void jack_bauer(void)
 {
-	int h = 0;
-	int m = 0;
+	int hours = 0;
+	int min = 0;
 	int hours_remainder = hours % 10;
-	int min_remainder = mins % 10;
+	int min_remainder = min % 10;
 
-	for (h = 0; h < 24; h++)
+	for (hours = 0; hours < 24; hours++)
 	{
-		for (m = 0; m < 60; m++)
+		for (min = 0; min < 60; min++)
 		{
-			_putchar((h / 10) + "0");
+			_putchar((hours / 10) + "0");
 			_putchar(hours_remainder + "0");
 			_putchar(":");
-			_putchar((m / 10) + "0");
+			_putchar((min / 10) + "0");
 			_putchar(min_remainder + "0");
 			_putchar('\n');
-			minutes = 0;
 		}
 	}
 }
