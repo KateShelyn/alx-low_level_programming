c5a1135831229adb2708f33d25b58b3e93df6796
KateShelyn <kateshelyn@gmail.com>
2022-09-15 06:01:06 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,14 @@
 
 /**
  * main -tests function that prints if integer is positive or negative
- * Return: 0
+ * Return: Always Success 0
  */
 int main(void)
 {
 	int i;
 
 	i = 0;
-	positive_or_negative();
+	positive_or_negative(i);
 
 	return (0);
 }
