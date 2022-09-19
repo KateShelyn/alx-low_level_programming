f7d26a900b6f2b5f5a0ed79bf723b855e8a095e9
KateShelyn <kateshelyn@gmail.com>
2022-09-19 15:51:04 -0700

    task

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,14 +4,14 @@
  * _strlen - a function
  * Description - using _strlen function
  * @s: string
- * Return: 0
+ * Return: length for integer
  */
 
 int _strlen(char *s)
 {
 	int len = 0;
 
-	while (*(s = len) != '\0')
+	while (*(s + len) != '\0')
 		len++;
 
 	return (len);
