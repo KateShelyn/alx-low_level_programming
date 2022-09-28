92500dea84a5c9e2f24ab9e6d245a1cf2b41026d
KateShelyn <kateshelyn@gmail.com>
2022-09-28 06:45:13 -0700

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,19 +5,19 @@
  * Description - using _strncat function
  * @dest: destination
  * @src: source
+ * @n: integer
  * Return: dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0,
-	    dest_len = 0;
+	int index = 0,dest_len = 0;
 
-	while (dest(index++))
+	while (dest[index++])
 		dest_len++;
 
-	for (index = 0; src(index); index++)
-		dest(dest_len++) = src(index);
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
