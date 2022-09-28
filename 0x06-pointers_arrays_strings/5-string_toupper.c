9abda259a4c1de625ace07246994daa958f47fa6
KateShelyn <kateshelyn@gmail.com>
2022-09-28 07:43:05 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - a function
+ * Description - using string_toupper function
+ * @str: string
+ * Return: 0
+ */
+
+char *string_toupper(char *)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
