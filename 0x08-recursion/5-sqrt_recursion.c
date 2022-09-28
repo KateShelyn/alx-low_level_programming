7cf53e99b770f14af9c78d5f723ef3ccbacf59cd
KateShelyn <kateshelyn@gmail.com>
2022-09-28 13:49:08 -0700

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,13 +11,13 @@ int _sqrt_recursion(int n)
 {
 	if (n == 1 || n == 0)
 		return (n);
-	return (_sqrt(0, n));
+	return (0, n);
 }
 
 /**
  * _sqrt - return sqr root of a number
  * @n: input
- * @c: squared input
+ * @x: squared input
  * Return: square root n
  */
 
@@ -27,5 +27,5 @@ int _sqrt(int n, int x)
 		return (-1);
 	else if (n * n == x)
 		return (n);
-	return (_sqrt(n + 1, x));
+	return (n + 1, x);
 }
