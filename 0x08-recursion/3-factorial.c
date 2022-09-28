99f15e8cc617d4061398f960a9abf62d2dc9452e
KateShelyn <kateshelyn@gmail.com>
2022-09-28 12:28:50 -0700

    3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - a function
+ * Description - using factorial function
+ * @n: input
+ * Return: 0
+ */
+
+int factorial(int n)
+{
+	int result = n;
+
+	if(n < 0)
+		return (-1);
+	else if (n >= 0 && n<= 1)
+		return (1);
+}
