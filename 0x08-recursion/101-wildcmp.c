64e183b3ebc79999ae7b9b64022fa3734ba0ebdf
KateShelyn <kateshelyn@gmail.com>
2022-09-28 13:02:04 -0700

    8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,16 +5,16 @@
  * Description - using wildcmp function
  * @s1: checked
  * @s2: pattern
- * Return : 1 if identical, 0 otherwise
+ * Return: 0 otherwise, one identical
  */
 
 int wildcmp(char *s1, char *s2)
 {
 	if (*s2 == '\0')
-		return ( *s1 == '\0');
+		return (*s1 == '\0');
 	if (*s2 == *s1)
-		return (*s1 != '\0' && wildcmp (s1 + 1, s2 + 1));
+		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp (s1 + 1, s2)));
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
 	return (0);
 
