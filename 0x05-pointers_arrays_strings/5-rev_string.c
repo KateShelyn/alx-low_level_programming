6c7ac7e8fcf7bdd73cbcfc5a4b9078eb91db3a2d
KateShelyn <kateshelyn@gmail.com>
2022-09-28 02:47:41 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,2 +1,17 @@
-#include 
-#include
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - a function that reverses
+ * Description - using rev_string function
+ * @s: string reverse
+ * Return: s
+ */
+
+void rev_string(char *s)
+{
+	int rev = s;
+
+	while (rev--)
+		putchar(*(s + rev));
+}
