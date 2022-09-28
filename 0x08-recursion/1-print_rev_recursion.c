fa8858289f75b4f92d78c5aa1393e63c175f1e3c
KateShelyn <kateshelyn@gmail.com>
2022-09-28 12:53:02 -0700

    2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,11 +4,12 @@
  * _print_rev_recursion - a function
  * Description - using _print_rev_recursion function
  * @s: string
+ * Return: void
  */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
