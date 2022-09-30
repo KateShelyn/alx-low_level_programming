aae675ffaf1f1ad11a8bd99586622fa933512c45
KateShelyn <kateshelyn@gmail.com>
2022-09-30 05:38:27 -0700

    0

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,6 +1,13 @@
 #include "main.h"
 
-int main(void)
+/**
+ * _puts - a function
+ * Description: using _puts function
+ *
+ * Return: 0
+ */
+
+void _puts(char *s)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
